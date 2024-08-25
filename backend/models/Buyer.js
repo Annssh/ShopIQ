e790a1af68fd566fd45e2282697f6adc248323c5
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-08-25 14:14:11 +0530

    Add bcrypt, generate token functionality

--- backend/models/Buyer.js
+++ backend/models/Buyer.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const buyerSchema = new mongoose.Schema({
     name: {
@@ -36,9 +37,53 @@ const buyerSchema = new mongoose.Schema({
         landmark: String,
         customerName: String,
         customerPhone: String
-    }]
+    }],
+    refreshToken: {
+        type: String
+    }
 },
     { timestamps: true }
 )
+
+// Before saving password just hash it
+buyerSchema.pre("save", async function (next) {
+    if (!this.isModified("password"))
+        return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+// compare passwore using bcrypt utility
+buyerSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// cookies-token generator
+buyerSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        // create a payload
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+buyerSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const Buyer = mongoose.model('Buyer', buyerSchema)
