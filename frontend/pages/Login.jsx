ec3d12fd96e7571128176ad5175ede6803d776c9
Ansh Agrawal <agrawalansh777@gmail.com>
2024-08-26 11:26:45 +0530

    Add Google Sign-in Option

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import ebg from "../images/ebg.jpg";
 
 const Login = () => {
   return (
-    <div className="">
+    <div>
       <div
         className="flex items-center justify-center min-h-screen bg-cover bg-no-repeat bg-center"
-        style={{ backgroundImage: `url('https://cdn.leonardo.ai/users/08f3e992-5a31-4466-ba32-3916fb1c36dd/generations/94b5de12-8e54-4de7-aa7a-673c4b1197fd/Leonardo_Phoenix_A_vibrant_and_modern_background_image_for_an_3.jpg')` }}
+        style={{
+          // backgroundImage: `url('https://img.freepik.com/free-photo/arrangement-black-friday-shopping-carts-with-copy-space_23-2148667047.jpg?w=996&t=st=1724649427~exp=1724650027~hmac=baeb940aef9214d86c043442d50e9254cf0ce8112b916e5ccecc6ec9bb2b9d03')`,
+          backgroundImage: `url(${ebg})`,
+        }}
       >
-        <div className="w-full max-w-md p-8 space-y-6 backdrop-blur rounded-lg shadow-lg">
-          <h2 className="text-3xl font-bold text-center text-gray-800">
+        <div className="w-full max-w-md p-8 space-y-6 rounded-lg bg-[#0e0e0e] bg-opacity-50">
+          <h2 className="text-3xl font-bold text-center text-white/80">
             Sign in to your account
           </h2>
           <form className="mt-8 space-y-6">
@@ -23,7 +29,7 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-600 placeholder-white/80 text-white/80 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm bg-black"
                   placeholder="Email address"
                 />
               </div>
@@ -37,12 +43,24 @@ const Login = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-600 placeholder-white/80 text-white/80 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm bg-black"
                   placeholder="Password"
                 />
               </div>
             </div>
-
+            <div>
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  const credentialResponseDecoded = jwtDecode(
+                    credentialResponse.credential
+                  );
+                  console.log(credentialResponseDecoded);
+                }}
+                onError={() => {
+                  console.log("Error in Login");
+                }}
+              />
+            </div>
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <input
@@ -53,7 +71,7 @@ const Login = () => {
                 />
                 <label
                   htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  className="ml-2 block text-sm text-white/80 hover:text-white"
                 >
                   Remember me
                 </label>
@@ -62,7 +80,7 @@ const Login = () => {
               <div className="text-sm">
                 <a
                   href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                  className="font-medium text-white/60 hover:text-white"
                 >
                   Forgot your password?
                 </a>
@@ -72,7 +90,7 @@ const Login = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm rounded-md text-white font-bold text-ellipsis bg-primary hover:bg-[#00090e] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
               >
                 Sign in
               </button>
